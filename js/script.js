777141057a55e7930e08f5e1bc1d5bb7acde8f5c
zmeller <zmeller@students.northweststate.edu>
2022-09-27 18:24:49 -0400

    k

---
+++ js/script.js
@@ -0,0 +1,15 @@
+//single line commment//
+
+/*multi line
+multi line*/
+
+console.log("Zachary Meller");
+console.log("My favorite food is Steak")
+
+console.log(2022);
+let text = "Supercalifragilisticexpialidocious";
+text.length
+console.log(text.length);
+
+let a = 2;
+console.log(a);
